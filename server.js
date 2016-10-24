770d0d85d298525ccccf60ea0b1151c36f024b32
Matt Latham <latham75@gmail.com>
2016-10-24 10:22:05 -0500

    test of server.js with index.pug file

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 
 const express = require('express')
 const bodyParser = require('body-parser')
-const routes = require('/routes/index')
+const routes = require('./routes/index')
 
 const app = express()
 
