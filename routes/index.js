db7534d2849df6487322e416b1cf33c5c4656131
Matt Latham <latham75@gmail.com>
2016-10-24 11:30:53 -0500

    claim post working

--- routes/index.js
+++ routes/index.js
@@ -8,25 +8,25 @@ router.get("/", (req, res) => {
   res.render('index')
 })
 
-router.get('/claim', (req, res, err) => {
-  Promise
-    .all([
-      Vehicles.find().sort({name: 1}),
-      Parts.find().sort({name: 1}),
-      Labor.find().sort({name: 1})
-    ])
-})
+// router.get('/claim', (req, res, err) => {
+//   Promise
+//     .all([
+//       Vehicles.find().sort({name: 1}),
+//       Parts.find().sort({name: 1}),
+//       Labor.find().sort({name: 1})
+//     ])
+// })
 
 
 router.post('/claim', (req, res, err) => {
   Claim
     .create(req.body)
-    // .then(() => res.redirect('/confirm'))
+    .then(() => res.redirect('/confirm'))
     .catch(err)
 })
 
 router.get('/confirm', (req, res) => {
-  res.render('confirm', {page: 'Claim'})
+  res.render('index')
 })
 
 router.get("/404", (req, res) => {
