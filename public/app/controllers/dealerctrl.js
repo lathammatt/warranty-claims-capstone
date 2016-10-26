111833a505926f2bcba5d659d00b93419444dd19
Matt Latham <latham75@gmail.com>
2016-10-26 15:44:09 -0500

    quick clear of menu if brand changes

--- public/app/controllers/dealerctrl.js
+++ public/app/controllers/dealerctrl.js
@@ -12,6 +12,7 @@ app.controller("DealerCtrl", function($scope, $http) {
     })
 
   $scope.brandSelect = (brand) => {
+    $scope.dealerList = []
     selection = brand
     console.log(selection)
 
