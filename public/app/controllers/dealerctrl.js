4a3d3fa98154269ca9e6adb423bd67ec11d40311
Matt Latham <latham75@gmail.com>
2016-10-26 17:22:47 -0500

    added factory for var storage, fixed select change

--- public/app/controllers/dealerctrl.js
+++ public/app/controllers/dealerctrl.js
@@ -1,29 +1,36 @@
 'use strict';
 
-app.controller("DealerCtrl", function($scope, $http) {
-  $scope.dealers = null
+app.controller("DealerCtrl", function($scope, $http, DataFactory) {
+  let shops = {}
   $scope.dealerList = []
   let selection = null
 
   $http.get('/api/dealer')
     .then(({ data: dealers }) => {
-      $scope.dealers = dealers
-      console.log($scope.dealers)
+      shops = dealers
+      console.log(shops)
     })
 
   $scope.brandSelect = (brand) => {
+    console.log(shops)
+    // DataFactory.setDealer(null)
     $scope.dealerList = []
     selection = brand
     console.log(selection)
 
-    for (let i = 0; i < $scope.dealers.length; i++) {
-      console.log("check", $scope.dealers[i].name, selection)
-      if ($scope.dealers[i].brand === selection) {
-        console.log($scope.dealers[i].name)
-        $scope.dealerList.push($scope.dealers[i].name)
+    for (let i = 0; i < shops.length; i++) {
+      console.log("check", shops[i].name, selection)
+      if (shops[i].brand === selection) {
+        console.log(shops[i].name)
+        $scope.dealerList.push(shops[i].name)
         console.log($scope.dealerList)
       }
     }
   }
 
+  $scope.dealerChoice = (choice) => {
+    console.log(choice)
+    console.log(shops)
+    DataFactory.setDealer(choice)
+  }
 })
