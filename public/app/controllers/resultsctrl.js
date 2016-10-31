5ef091748a4fe5b05c3b4ab035e6e463b1063f9d
Matt Latham <latham75@gmail.com>
2016-10-31 11:30:59 -0500

    history IDs in DOM sorted by dealer

--- public/app/controllers/resultsctrl.js
+++ public/app/controllers/resultsctrl.js
@@ -1,13 +1,28 @@
 'use strict';
 
-app.controller('ResultsCtrl', function($scope, DataFactory){
+app.controller('ResultsCtrl', function($scope, DataFactory) {
+
+  $scope.claimsList = []
+
 
   const loadClaim = () => {
-  $scope.finalClaim = DataFactory.getClaimDraft()
+    $scope.finalClaim = DataFactory.getClaimDraft()
   }
 
+
+  const loadHistory = () => {
+    let user = DataFactory.getDealer()
+    DataFactory.getAll()
+      .then((claims) => {
+        for (let i = 0; i < claims.data.length; i++) {
+          if (claims.data[i].dealer === user) {
+            $scope.claimsList.push(claims.data[i]._id)
+          }
+        }
+      })
+  }
+
+
   loadClaim()
-
-
-
+  loadHistory()
 })
